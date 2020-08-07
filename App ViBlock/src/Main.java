6097232304f8adfa32db4e103449957fd1f1191d
LM095 <luca.marzari@studenti.univr.it>
2020-08-08 00:14:51 +0200

    tests

--- App ViBlock/src/Main.java
+++ App ViBlock/src/Main.java
@@ -1,21 +1,10 @@
 import Utils.DatabaseConnection;
 import Utils.StageManager;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 
 public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage)
-    {
-       StageManager loginPageState = new StageManager();
-       loginPageState.setStageHomepage(primaryStage);
-    }
-
 
     public static void main(String[] args) {
         launch(args);
@@ -24,5 +13,12 @@ public class Main extends Application {
 
         if(db != null)
             db.DBCloseConnection();
+    }
+
+    @Override
+    public void start(Stage primaryStage)
+    {
+        StageManager loginPageState = new StageManager();
+        loginPageState.setStageHomepage(primaryStage);
     }
 }
